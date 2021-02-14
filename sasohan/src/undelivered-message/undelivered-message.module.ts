357e02d3cb159441cc12a6b6f8fcf94ff068efbd
이다빈 <dabeen@idabin-ui-MacBookPro.local>
2021-02-14 23:38:01 +0900

    create controller, provider, module

---
+++ sasohan/src/undelivered-message/undelivered-message.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UndeliveredMessage } from 'src/entity/undeliveredMessage.entity';
+import { UndeliveredMessageController } from './undelivered-message.controller';
+import { UndeliveredMessageService } from './undelivered-message.service';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([UndeliveredMessage])],
+  controllers: [UndeliveredMessageController],
+  providers: [UndeliveredMessageService]
+})
+export class UndeliveredMessageModule {}
