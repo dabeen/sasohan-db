1db6fab186e070ca33100500062aa21d28ef0e38
이다빈 <dabeen@idabin-ui-MacBookPro.local>
2021-02-25 00:57:17 +0900

    modify CRUD API

--- sasohan/src/review/review.controller.ts
+++ sasohan/src/review/review.controller.ts
@@ -1,42 +1,126 @@
 import { Body, Controller, Delete, Get, HttpStatus, Param, Post, Put, Res } from '@nestjs/common';
-import { ReviewDto } from 'src/dto/review.dto';
-import { Review } from '../entity/review.entity';
+import { ReviewDTO, UpdateReviewDTO } from 'src/dto/review.dto';
 import { ReviewService } from './review.service';
 
 @Controller('review')
 export class ReviewController {
   constructor(private readonly reviewService: ReviewService){}
  
-    @Get('all')
-    async getAll():Promise<Review[]>{
-        return await this.reviewService.findAll();
+  @Get('all')
+    async getAllReview() {
+        // this returns all Review in sasohan database 
+        try {
+            const allReview = await this.reviewService.getAll();
+            return {
+              review: allReview,
+              success: true
+            }
+        } catch (e) {
+            return {
+              success : false,
+              error_msg: e
+            }
+        }
     }
 
+    @Get("/written/:user_id") 
+    async getAllUserReview(@Param("user_id") user_id: string) {
+        // this retruns all posts written by users and whether they are successful.
 
-    @Post('register')
-    async registerReview(@Res() res, @Body() createReviewDto: ReviewDto) {
-        const newReview = await this.reviewService.createOneReview(createReviewDto);
-        return res.status(HttpStatus.OK).json({
-            review: newReview,
-            querySuccess: true,
-        });
+        try {
+            const result = await this.reviewService.getAllUserWrittenReview(user_id);
+            return {
+              review: result,
+              success: true
+            }
+        } catch (e) {
+            return {
+                success : false,
+                error_msg: e
+            }
+        }
     }
 
-    @Get(":id") 
-    async getOne(@Param("id") Review_id: string) {
-        const result = await this.reviewService.getOne(Review_id);
-        return result;
+    @Get("/toWrite/:user_id")
+    async getToWriteReview(@Param("user_id") user_id: string) {
+
+        try {
+            const result = await this.reviewService.getToWriteReviews(user_id)
+            return {
+                post : result,
+                success: true
+            }
+        } catch (e) {
+            return {
+                success : false,
+                error_msg: e
+            }
+        }
+        
+    }
+
+    @Get(":id")
+    async getOneReview(@Param("id") review_id: string) {
+        // this returns one Review in sasohan database
+        try {
+            const result = await this.reviewService.getOne(review_id);
+            return {
+              review: result,
+              success: true
+            }
+        } catch (e) {
+            return {
+                success : false,
+                error_msg: e
+            }
+        }
+
+    }
+    
+    @Post("/register")
+    async registerOneReview(@Body()createReviewDTO: ReviewDTO) {
+        // This returns the success of creating (a) new Review(s) and returns an error message if an error occurs.
+        try {
+            await this.reviewService.createOneReview(createReviewDTO);
+            return {success : true}
+        } catch (e) {
+            return {
+                success: false,
+                error_msg: e
+            }
+        }
+    }
+
+    // db에 없는 경우 error를 반환하지 않음 -> 수정해야함
+    @Delete(":id")
+    async deleteOneReview(@Param("id") review_id: string) {
+        // This returns the success of the Review deletion, and returns an error message if an error occurs.
+        try {
+            await this.reviewService.deleteOneReview(review_id);
+            return { success : true }
+        } catch (e) {
+            return {
+                success: false,
+                error_msg: e
+            }
+        }
     }
 
     @Put(":id")
-    async setOne(@Param("id") Review_id: string, @Body() updateReviewDto: ReviewDto) {
-        const result = await this.reviewService.updateOneReview(Review_id, updateReviewDto);
-        return result; 
+    async updateOneReview(@Param("id") review_id: string, @Body() updateReviewDTO: UpdateReviewDTO) {
+        // This returns whether the Review update is successful or not, and returns an error message if an error occurs.
+        try {
+            await this.reviewService.updateOneReview(review_id, updateReviewDTO)
+            return {
+                success: true
+            }
+        } catch (e) {
+            return {
+                success: false,
+                error_msg: e
+            }
+        }
     }
 
-    @Delete(":id")
-    async deleteOne(@Param("id") Review_id: string) {
-        const ret = await this.reviewService.deleteOneReview(Review_id);
-        return ret;
-    }
+
 }
--- sasohan/src/review/review.service.ts
+++ sasohan/src/review/review.service.ts
@@ -1,14 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { ReviewDto } from 'src/dto/review.dto';
-import { Repository } from 'typeorm';
+import { ReviewDTO, UpdateReviewDTO } from 'src/dto/review.dto';
+import { Posts } from 'src/entity/posts.entity';
+import { Resolver } from 'src/entity/resolver.entity';
+import { getConnection, Repository } from 'typeorm';
 import { Review } from '../entity/review.entity';
 
 @Injectable()
 export class ReviewService {
   constructor(@InjectRepository(Review) private reviewRepo: Repository<Review>) {}
 
-  findAll(): Promise<Review[]> {
+  getAll(): Promise<Review[]> {
     return this.reviewRepo.find();
   }
 
@@ -16,15 +18,41 @@ export class ReviewService {
     return this.reviewRepo.findOne(id);
   }
 
-  createOneReview(ReviewDto: ReviewDto): Promise<ReviewDto> {
-    return this.reviewRepo.save(ReviewDto);
+  createOneReview(ReviewDTO: ReviewDTO): Promise<ReviewDTO> {
+    return this.reviewRepo.save(ReviewDTO);
   }
 
   deleteOneReview(id: string) {
     return this.reviewRepo.delete(id);
   }
 
-  updateOneReview(id: string, updateReviewDto: ReviewDto) {
-    return this.reviewRepo.update(id, updateReviewDto);
+  updateOneReview(id: string, updateReviewDTO: UpdateReviewDTO) {
+    return this.reviewRepo.update(id, updateReviewDTO);
+  }
+
+  async getAllUserWrittenReview(user_id: string) {
+    
+    const review = await getConnection().createQueryBuilder()
+    .select("review")
+    .from(Review, "review")
+    .where("review.user_id = :user_id", {user_id: user_id})
+    .getMany()
+
+    return review;
+  }
+
+  // 리뷰를 작성하지 않은 게시물 정보
+  async getToWriteReviews(user_id: string) {
+
+    const review = await getConnection().createQueryBuilder()
+      .select("posts")
+      .from(Posts, "posts")
+      .leftJoin(Resolver, "resolver", "posts.post_id = resolver.post_id")
+      .where("posts.complete = true")
+      .andWhere("posts.user_id = :user_id", { user_id: user_id })
+      .where("posts.post_id not in (select post_id from review)").getMany();
+
+    return review
+
   }
 }
