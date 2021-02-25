0826fee76dfd5eed2a3c955c274f7c824fbfd35f
이다빈 <dabeen@idabin-ui-MacBookPro.local>
2021-02-26 01:11:56 +0900

    add class validation pipe

--- sasohan/src/dto/user.dto.ts
+++ sasohan/src/dto/user.dto.ts
@@ -1,22 +1,41 @@
-export class UserDTO {
+import { IsNumber, IsString } from "class-validator";
 
+export class UserDTO {
+  @IsString()
   user_id : string;
+
+  @IsString()
   nickname : string;
+
+  @IsString()
   gender : string;
+
+  @IsString()
   birthday : string;
+
+  @IsNumber()
   age : number;
+
+  @IsNumber()
   point : number; 
+
+  @IsString()
   account_type: string;
 }
 
 export class UpdateUserDTO {
+  @IsString()
+  gender ?: string;
 
-  gender : string;
-  birthday : string;
+  @IsString()
+  birthday ?: string;
+
+  @IsNumber()
   age : number;
-
+  
 }
 
 export class UpdateUserPointDTO {
+  @IsNumber()
   point : number;
 }
