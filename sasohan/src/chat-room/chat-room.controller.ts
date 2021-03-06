b35c289f456f1d905174105088820cc8c24ae78a
이다빈 <dabeen@idabin-ui-MacBookPro.local>
2021-03-07 00:56:18 +0900

    edit CRUD API in chat-room

--- sasohan/src/chat-room/chat-room.controller.ts
+++ sasohan/src/chat-room/chat-room.controller.ts
@@ -1,5 +1,5 @@
 import { Body, Controller, Delete, Get, HttpStatus, Param, Post, Put, Res } from '@nestjs/common';
-import { ChatRoomDto } from 'src/dto/chat-room.dto';
+import { ChatRoomDTO } from 'src/DTO/chat-room.dto';
 import { ChatRoom } from '../entity/chat-room.entity';
 import { ChatRoomService } from './chat-room.service';
 
@@ -7,43 +7,50 @@ import { ChatRoomService } from './chat-room.service';
 export class ChatRoomController {
   constructor(private readonly chatRoomService: ChatRoomService){}
  
-    @Get('all')
-    async getAll():Promise<ChatRoom[]>{
-        return await this.chatRoomService.findAll();
+  @Get(":id")
+    async getOneCategory(@Param("id") chat_room_id: string) {
+        // this returns one category in sasohan database
+        try {
+            const result = await this.chatRoomService.getOne(chat_room_id);
+            return {
+                category: result,
+                success: true
+            }
+        } catch (e) {
+            return {
+                success : false,
+                error_msg: e
+            }
+        }
+
     }
 
-    @Post()
-    async create(@Body() createChatRoomDto: ChatRoomDto) {
-        const result = await this.chatRoomService.createOneChatRoom(createChatRoomDto);
-        return result;
-    }
-
-    @Post('register')
-    async registerChatRoom(@Res() res, @Body() createChatRoom: ChatRoomDto) {
-        const newChatRoom = await this.chatRoomService.createOneChatRoom(createChatRoom);
-        return res.status(HttpStatus.OK).json({
-            post: newChatRoom,
-            querySuccess: true,
-        });
-    }
-
-    @Get(":id") 
-    async getOne(@Param("id") chatRoom_id: string) {
-        const result = await this.chatRoomService.getOne(chatRoom_id);
-        return result;
-    }
-
-    @Put(":id")
-    async setOne(@Param("id") chatRoom_id: string, @Body() updateChatRoomDto: ChatRoomDto) {
-        const result = await this.chatRoomService.updateOneChatRoom(chatRoom_id, updateChatRoomDto);
-        return result; 
+    @Post("store")
+    async registerOneCategory(@Body()createChatRoomDTO: ChatRoomDTO) {
+        // This returns the success of creating (a) new category(s) and returns an error message if an error occurs.
+        try {
+            await this.chatRoomService.createOneChatRoom(createChatRoomDTO);
+            return {success : true}
+        } catch (e) {
+            return {
+                success: false,
+                error_msg: e
+            }
+        }
     }
 
     @Delete(":id")
-    async deleteOne(@Param("id") chatRoom_id: string) {
-        const ret = await this.chatRoomService.deleteOneChatRoom(chatRoom_id);
-        return ret;
+    async deleteOneCategory(@Param("id") chat_room_id: string) {
+        // This returns the success of the category deletion, and returns an error message if an error occurs.
+        try {
+            await this.chatRoomService.deleteOneChatRoom(chat_room_id);
+            return { success : true }
+        } catch (e) {
+            return {
+                success: false,
+                error_msg: e
+            }
+        }
     }
-
 
 }
